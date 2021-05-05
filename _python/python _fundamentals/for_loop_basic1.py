4ccee0224144f5bfd7a63c6496245ec5cbb6f9ae
Ibraheem <ibraheem.shwiki@gmail.com>
2021-05-05 14:26:02 +0300

    loop asic 1

---
+++ _python/python _fundamentals/for_loop_basic1.py
@@ -0,0 +1,30 @@
+# for i in range(15):
+#     print(i)
+
+# for i in range(5, 1000, 5):
+#     print (i)
+
+# for i in range(1, 100, 1):
+#     if i % 5 == 0 :
+#         print("dojo")
+#     if i % 10 == 0 :
+#         print("coding-dojo")    
+#     else :
+#          print(i)
+
+# sum = 0
+# for i in range(500000):
+#     if i % 2 != 0 :
+#         sum = sum + i
+# print(sum)        
+
+
+# for i in range(2018, 0, -4):
+#     print(i)
+
+
+# lowNum = 2
+# highNum = 30
+# mult = 3
+# for i in range(lowNum, highNum, mult):
+#     print(i)
