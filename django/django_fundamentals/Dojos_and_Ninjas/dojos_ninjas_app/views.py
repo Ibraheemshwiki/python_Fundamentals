d7cb44ded515a3ed6d3afc59c6606336ac1c2e63
Ibraheem <ibraheem.shwiki@gmail.com>
2021-05-23 23:44:45 +0300

    dojos and ninjas

--- django/django_fundamentals/Dojos_and_Ninjas/dojos_ninjas_app/views.py
+++ django/django_fundamentals/Dojos_and_Ninjas/dojos_ninjas_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+def index(request):
+    context = {
+        'dojos' : dojo.objects.all(),
+        'ninjas' : ninja.objects.all()
+    }
+    return render(request, 'index.html', context)
 
-# Create your views here.
+def add_dojo(request):
+    dojo.objects.create(name = request.POST['name'], city = request.POST['city'], state = request.POST['state'])
+    return redirect('/')
+
+def add_ninja(request):
+    ninja.objects.create(first_name = request.POST['fname'], last_name = request.POST['lname'], dojo= dojo.objects.get(name = request.POST['dojo_select']))
+
+    return redirect('/')
